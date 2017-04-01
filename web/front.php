d79bc3a34232297ffa91f852f44b694eb9679c0b
Syed Huda <syed.huda@gmail.com>
2017-04-01 11:29:47 +0100

    Added routing component and moved routes to own file.

--- web/front.php
+++ web/front.php
@@ -2,25 +2,28 @@
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
 
 require_once __DIR__.'/../vendor/autoload.php';
 
 $request = Request::createFromGlobals();
-$response = new Response();
+$routes = include __DIR__.'/../src/app.php';
 
-$map = [
-    '/hello' => 'hello',
-    '/bye' => 'bye',
-];
+$context = new RequestContext();
+$context->fromRequest($request);
+$matcher = new UrlMatcher($routes, $context);
 
-$path = $request->getPathInfo();
-if (isset($map[$path])) {
+try {
+    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
     ob_start();
-    extract($request->query->all(), EXTR_SKIP);
-    include sprintf(__DIR__.'/../src/pages/%s.php', $map[$path]);
+    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
     $response = new Response(ob_get_clean());
-} else {
+} catch (ResourceNotFoundException $e) {
     $response = new Response('Not Found', 404);
+} catch (Exception $e) {
+    $response = new Response('An error occurred', 500);
 }
 
 $response->send();
