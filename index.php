5f50fe1b8fd228334bb3205b6582c918a07cc6b1
Syed Huda <syed.huda@gmail.com>
2017-04-01 10:35:08 +0100

    Create bye.php and extract common code to init.php

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
 <?php
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+require_once __DIR__.'/init.php';
 
-require_once __DIR__.'/vendor/autoload.php';
-
-$request = Request::createFromGlobals();
 $input = $request->get('name', 'World');
-$response = new Response(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
+
+$response->sendContent(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
 $response->send();
