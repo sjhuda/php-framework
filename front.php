d45ae73d4d4747b797eb50c00f2d0746f3736969
Syed Huda <syed.huda@gmail.com>
2017-04-01 10:45:54 +0100

    Created new front script and added hello script.

---
+++ front.php
@@ -0,0 +1,24 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+require_once __DIR__.'/vendor/autoload.php';
+
+$request = Request::createFromGlobals();
+$response = new Response();
+
+$map = [
+    '/hello' => __DIR__.'/hello.php',
+    '/bye' => __DIR__.'/bye.php'
+];
+
+$path = $request->getPathInfo();
+if (isset($map[$path])) {
+    require $map[$path];
+} else {
+    $response->setStatusCode(404);
+    $response->setContent('Not Found');
+}
+
+$response->send();
