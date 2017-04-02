2c360f1927b11dbce01dfd32fb2cad3e56e34a24
Syed Huda <syed.huda@gmail.com>
2017-04-02 07:19:46 +0100

    Added template renderer.

--- web/front.php
+++ web/front.php
@@ -8,6 +8,14 @@ use Symfony\Component\Routing\RequestContext;
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+function render_template($request) {
+    extract($request->attributes->all(), EXTR_SKIP);
+    ob_start();
+    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
+
+    return new Response(ob_get_clean());
+}
+
 $request = Request::createFromGlobals();
 $routes = include __DIR__.'/../src/app.php';
 
@@ -16,10 +24,8 @@ $context->fromRequest($request);
 $matcher = new UrlMatcher($routes, $context);
 
 try {
-    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
-    ob_start();
-    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
-    $response = new Response(ob_get_clean());
+    $request->attributes->add($matcher->match($request->getPathInfo()));
+    $response = call_user_func($request->attributes->get('_controller'), $request);
 } catch (ResourceNotFoundException $e) {
     $response = new Response('Not Found', 404);
 } catch (Exception $e) {
